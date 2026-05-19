26c07228a8592acdab77b15a6f7135d1f97b2128
ShivamDholeriya <shivamdholeriya3@gmail.com>
2026-05-19 14:48:10 +0530

    Added Procfile and updated CORS

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,3 @@
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routes.compare import router as compare_router
@@ -8,18 +7,13 @@ app = FastAPI(title="Plagiarism Detector API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 app.include_router(auth_router, prefix="/api/auth")
 app.include_router(compare_router, prefix="/api")
-
 
 @app.get("/")
 def home():
